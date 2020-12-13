14845f0dcc68e4197c57d78001845ee47480fdd0
slimit75 <limitspeed75@gmail.com>
2020-12-12 19:24:36 -0500

    Real inital commit

---
+++ Visual Studio/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Documents;
+
+namespace AERoundtripCalc
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Calculate_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string route = new TextRange(InRoute.Document.ContentStart, InRoute.Document.ContentEnd).Text;
+                string aircraft = new TextRange(InAircraft.Document.ContentStart, InAircraft.Document.ContentEnd).Text;
+                double raw = (Convert.ToDouble(route) / Convert.ToDouble(aircraft)) * 7;
+
+                raw = Math.Ceiling(raw);
+
+                Out.Text = raw.ToString();
+            }
+            catch (System.FormatException)
+            {
+                Out.Text = "Check #'s";
+            }
+        }
+    }
+}
